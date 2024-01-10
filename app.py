d5ec2e5f1e638b52804a925cf3bde84d94dfcb9d
luoyi99 <luoyi@uw.edu>
2024-01-09 17:32:27 -0800

    added more information

--- app.py
+++ app.py
@@ -16,26 +16,22 @@ col1, col2 = st.columns(2)
 
 # about
 col1.subheader("About",divider = "grey")
-col1.text("I am a UX designer who graduated from the\nUniversity of California, Irvine,\
-            and currently working\n at VIVO as a UX designer on IoT and IoV-related products.\n\
-            My design approach is one that values research, collaboration, testing, and iteration,\n while balancing business goals and user needs.  \
+col1.markdown("I am a UX designer who graduated from the University of California, Irvine,\
+            and previously working at VIVO as a UX designer on IoT and IoV-related products.\
+            My design approach is one that values research, collaboration, testing, and iteration, while balancing business goals and user needs.  \
             I am open to new opportunities that can help expand my understanding of how design can help amplify human experiences.")
 
 # education
 col2.subheader("Education",divider = "grey")
-col2.text("B.S. in Informatics & B.S. in Mathematics")
-col2.text("@ University of California, Irvine")
+col2.markdown("B.S. in Informatics & B.S. in Mathematics **@ University of California, Irvine**")
+col2.markdown("M.S. in Technology Innovation **@ University of Washington**")
 
 # work experience
 col2.subheader("Work Experience",divider = "grey")
-col2.text("UX Designer @ VIVO")
-
-# work experience
-col2.subheader("Work Experience",divider = "grey")
-col2.text("vivo - UX designer")
+col2.markdown("UX Designer **@ VIVO**")
 
 # hobbies and interests
 col2.subheader("Hobbies and Interests",divider = "grey")
-col2.text("stand-up comedy / travelling / food")
+col2.markdown("stand-up comedy / travelling / food")
 
 
